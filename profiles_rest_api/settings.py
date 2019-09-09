0274f6fcd04e29ed44990d61d1eaec9bae3d3024
Areeb Ahmed <areeb_aaa@outlook.com>
2019-09-09 08:23:01 +0500

    Configured for deployment.

--- profiles_rest_api/settings.py
+++ profiles_rest_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8g#ccvsxh$*eg6%j3s$)=36khs4u7rc$!da=8)dm(t8iour7d='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -124,3 +124,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
